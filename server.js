b55405b2b24a7c79d8eb96531c663ece92e96c7f
Alex Forss <alexforss@jvf-imac.fritz.box>
2021-01-01 19:28:57 -0800

    Updated Table Headers

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const config = {
   auth0Logout: true
 };
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
   config.baseURL = `http://localhost:${port}`;
 }
